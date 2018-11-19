fdeb143127227cdaab867f087ed4ecd8f08650b3
liucongactivity <lceclipes@outlook.com>
2018-11-19 21:40:36 +0800

    修改了fragment_5内容

--- app/src/main/java/com/mad/trafficclient/fragment/Fragment_5.java
+++ app/src/main/java/com/mad/trafficclient/fragment/Fragment_5.java
@@ -11,7 +11,7 @@ import com.mad.trafficclient.R;
 public class Fragment_5 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_guide, container, false);
+        View view = inflater.inflate(R.layout.fragment_layout05, container, false);
         return view;
     }
 }
