dfac7930ceee7b0ed749def1e194721eb5364d64
XM2510136957 <2510136957@qq.com>
2018-11-19 21:18:55 +0800

    新增加fragment_5

---
+++ app/src/main/java/com/mad/trafficclient/fragment/Fragment_5.java
@@ -0,0 +1,14 @@
+package com.mad.trafficclient.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_5 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
