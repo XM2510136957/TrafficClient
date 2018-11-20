9090ae3c8eb97e191012c11d0af54f1f85915cfc
liucongactivity <lceclipes@outlook.com>
2018-11-20 19:14:25 +0800

    完成Fragment01模块

--- app/src/main/java/com/mad/trafficclient/Bean/Billing.java
+++ app/src/main/java/com/mad/trafficclient/Bean/Billing.java
@@ -1,4 +1,31 @@
 package com.mad.trafficclient.Bean;
 
 public class Billing {
+    private int carId;
+    private int recharge;
+    private String operator;
+    private String time;
+
+    public Billing(int carId, int recharge, String operator, String time) {
+        this.carId = carId;
+        this.recharge = recharge;
+        this.operator = operator;
+        this.time = time;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public int getRecharge() {
+        return recharge;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
