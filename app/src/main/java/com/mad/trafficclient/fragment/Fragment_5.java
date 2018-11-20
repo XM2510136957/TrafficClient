d389a7ea51fb52478214c0388365170266e8e601
LHM <2485695518@qq.com>
2018-11-20 09:36:40 +0800

    测试Fragment5的提交

--- app/src/main/java/com/mad/trafficclient/fragment/Fragment_5.java
+++ app/src/main/java/com/mad/trafficclient/fragment/Fragment_5.java
@@ -14,6 +14,7 @@ public class Fragment_5 extends Fragment {
         View view = inflater.inflate(R.layout.fragment_layout05, container, false);
         //提交修改
         //科米克vbvb
+        //jhh
         return view;
     }
 }
